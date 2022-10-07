58ec3488af8abb2374c7f0e3b7b74599a104105f
rostik <rostyslav.volosheniuk.ri.2022@lpnu.ua>
2022-10-07 10:13:24 +0300

    branch2_commit

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 int main()
 {
-	//setlocale(LC_ALL, "Ukrainian");
+	setlocale(LC_ALL, "Ukrainian");
 	double a;
 	double z1;
 	double z2;
-	cout << "ââåäiòü à = "; cin >> a;
+	//cout << "ââåäiòü à = ";
+	cin >> a;
 
 	z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * sin(2 * a) * sin(2 * a));
 	z2 = 2 * sin(a);
